5cde00db26c5b9fff0b10822ab5bfe6a4e55fc6e
arekop100 <arekop100@gmail.com>
2020-03-20 18:46:07 +0100

    xd

---
+++ src/main/java/pl/edu/pwsztar/domain/mapper/MovieMapper.java
@@ -0,0 +1,20 @@
+package pl.edu.pwsztar.domain.mapper;
+
+import org.springframework.stereotype.Component;
+import pl.edu.pwsztar.domain.dto.CreateMovieDto;
+import pl.edu.pwsztar.domain.entity.Movie;
+
+@Component
+public class MovieMapper {
+
+    public Movie mapToMovie(CreateMovieDto createMovieDto) {
+
+        Movie movie = new Movie();
+
+        movie.setTitle(createMovieDto.getTitle());
+        movie.setImage(createMovieDto.getImage());
+        movie.setYear(createMovieDto.getYear());
+
+        return movie;
+    }
+}
